31f3c981533149eb15f7506b84c0c5dc9a64c201
Shitcoder731 <shitanshu.731@gmail.com>
2024-05-03 19:28:41 +0530

    Add Links for sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -15,6 +20,19 @@ const Sidebar = ({ user }: SiderbarProps) => {
             className="size-[24px] max-xl:size-14 "
           />
         </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+              href={item.route}
+              key={item.label}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
