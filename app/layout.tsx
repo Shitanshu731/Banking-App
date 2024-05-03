634462a99c8369e22a23edb107017960582c441a
Shitcoder731 <shitanshu.731@gmail.com>
2024-05-03 16:31:44 +0530

    Implement iitial file and folder Structure

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Finance App",
+  description: "Mordern banking Platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
