c3f72b7c55e9945914a6a7355a63ff288d915b8f
Shitcoder731 <shitanshu.731@gmail.com>
2024-05-03 19:59:07 +0530

    Implement Left Sidebar Fully

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,29 +1,90 @@
+"use client";
 import React from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const MobileNav = ({ user }: MobileNavProps) => {
+  const pathname = usePathname();
   return (
-    <div>
+    <section className="w-full max-w-[264px]">
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetTrigger>
+          <Image
+            src={"/icons/hamburger.svg"}
+            alt=""
+            height={30}
+            width={30}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <nav className="flex flex-col gap-4">
+            <Link
+              href="/"
+              className="mb-12 cursor-pointer flex items-center gap-2 px-4 "
+            >
+              <Image src="/icons/logo.svg" alt="logo" width={34} height={34} />
+              <h1 className="text-26 font-ibm-plex-serif text-black-1 font-bold">
+                asdasd
+              </h1>
+            </Link>
+            <div className="mobilenav-sheet">
+              <SheetClose asChild>
+                <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                  {sidebarLinks.map((item) => {
+                    const isActive =
+                      pathname === item.route ||
+                      pathname.startsWith(`${item.route}/`);
+                    return (
+                      <SheetClose asChild key={item.route}>
+                        <Link
+                          className={cn("mobilenav-sheet_close w-full", {
+                            "bg-bank-gradient": isActive,
+                          })}
+                          href={item.route}
+                          key={item.label}
+                        >
+                          <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={cn({
+                              "invert-0 brightness-[3]": isActive,
+                            })}
+                          />
+
+                          <p
+                            className={cn("text-16 text-black font-semibold", {
+                              "!text-white": isActive,
+                            })}
+                          >
+                            {item.label}
+                          </p>
+                        </Link>
+                      </SheetClose>
+                    );
+                  })}
+                </nav>
+              </SheetClose>
+            </div>
+            USER
+          </nav>
         </SheetContent>
       </Sheet>
-    </div>
+    </section>
   );
 };
 
