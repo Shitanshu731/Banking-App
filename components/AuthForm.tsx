b14946c8f132d5d67624ca5d646d275221150f26
Shitcoder731 <shitanshu.731@gmail.com>
2024-05-04 18:29:27 +0530

    Add Shadcn form Component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,93 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const AuthForm = ({ type }: { type: string }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  const [user, setUser] = useState(null);
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8 ">
+        <Link href="" className="cursor-pointer items-center flex gap-1">
+          <Image src="/icons/logo.svg" width={34} height={34} alt="logo" />
+          <h1 className="text-26 font-ibm-plex-serif text-black-1">dsfsfdsf</h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1>
+            {user ? "Link Account" : type === "sign-in" ? "SignIn" : "SignUp"}
+            <p className="text-16 font-normal text-grat-600">
+              {user
+                ? "Link your Accont to get started"
+                : "Please Enter your details"}
+            </p>
+          </h1>
+        </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4"></div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
