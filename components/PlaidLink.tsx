51957ff331ce959ba6f2cadcc4bcd870d9020b2c
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-25 12:40:15 +0530

    Create functionality for ConnectBank button

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import {
+  PlaidLinkOnSuccess,
+  PlaidLinkOptions,
+  usePlaidLink,
+} from "react-plaid-link";
+import { useRouter } from "next/navigation";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const getLinkToken = async () => {};
+    getLinkToken();
+  }, []);
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: string) => {
+      // await exchangePublicToken({
+      //     publicToken : public_token,
+      //     user,
+      // })
+      router.push("/");
+    },
+    [user]
+  );
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
+  const { open, ready } = usePlaidLink(config);
   return (
     <>
       {variant === "primary" ? (
-        <Button className="plaidlink-primary">Connect Bank</Button>
+        <Button
+          onClick={() => open()}
+          disabled={!ready}
+          className="plaidlink-primary"
+        >
+          Connect Bank
+        </Button>
       ) : variant === "ghost" ? (
         <Button>Connect Bank</Button>
       ) : (
