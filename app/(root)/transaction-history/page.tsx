bd6606f004d885df45490c15e37ccd0dbe8e8b13
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-26 12:57:23 +0530

    Add accountName to transaction-history as Header.

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,7 +1,24 @@
 import HeaderBox from "@/components/HeaderBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const TransactionHistory = () => {
+const TransactionHistory = async ({
+  searchParams: { id, page },
+}: SearchParamProps) => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+  if (!accounts) return;
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+  console.log({
+    accountsData,
+    account,
+  });
   return (
     <div className="transactions">
       <div className="transactions-header">
@@ -9,6 +26,13 @@ const TransactionHistory = () => {
           title="Transaction History"
           subtext="see your bank details and transactions."
         />
+      </div>
+      <div className="space-y-6 ">
+        <div className="transactions-account">
+          <div className="flex flex-col gap-2">
+            <h2>{account?.data.name} </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
