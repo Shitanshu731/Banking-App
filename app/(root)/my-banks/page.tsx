d14c1ec12d19b6a774840bf338fbd0e8c742ab6a
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-26 13:16:15 +0530

    Add header for the MyBanks page.

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,7 +1,17 @@
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 
 const page = () => {
-  return <div>my Banks</div>;
+  return (
+    <div className="flex">
+      <div className="my-banks">
+        <HeaderBox
+          title="My Bank Accounts"
+          subtext="Seamlessly manage your banking activities."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default page;
