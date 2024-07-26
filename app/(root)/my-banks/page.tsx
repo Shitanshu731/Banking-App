55e354929efb0e771510f0296c2032d2dbd48777
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-26 13:24:20 +0530

    Add individual BankDetails MyBanks page.

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,7 +1,14 @@
+import BankCard from "@/components/BankCard";
 import HeaderBox from "@/components/HeaderBox";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
   return (
     <div className="flex">
       <div className="my-banks">
@@ -9,6 +16,19 @@ const page = () => {
           title="My Bank Accounts"
           subtext="Seamlessly manage your banking activities."
         />
+        <div className="space-y-4">
+          <h2 className="header-2">Your Cards</h2>
+          <div className="flex flex-wrap gap-6">
+            {accounts &&
+              accounts?.data.map((a: Account) => (
+                <BankCard
+                  key={accounts.id}
+                  account={a}
+                  userName={loggedIn?.firstName}
+                />
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
