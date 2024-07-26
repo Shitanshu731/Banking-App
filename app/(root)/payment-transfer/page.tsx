5afae7c9906adcea912e8d620ae9ea2b51e32db7
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-26 13:58:32 +0530

    Add fetch accounts and fetch LoggedIn user func to PaymentTransfer page.

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,6 +1,15 @@
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+  if (!accounts) return;
+  const accountsData = accounts?.data;
+
   return <div>Transfer</div>;
 };
 
